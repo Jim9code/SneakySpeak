28adaab97969a0a508ae68e0101ec3f9fd001601
jeth <rinfwang4@gmail.com>
2025-04-05 01:30:55 +0100

    jjjx

--- sneaky_backend/src/server.js
+++ sneaky_backend/src/server.js
@@ -7,9 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 // Create HTTP server
 const httpServer = createServer(app);
-app.get('/health', (_, res) => {
-    res.status(200).send('OK');
-  });
+
 // Start server immediately
 httpServer.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
